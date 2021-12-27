4f282e43ecb0f93d2e501d12831a2d23c1a13613
Tharun1828 <91731118+Tharun1828@users.noreply.github.com>
2021-12-27 05:41:23 +0530

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -46,7 +46,7 @@ async def storefile(c, m):
 
     # creating urls
     bot = await c.get_me()
-    url = f"https://t.me/{bot.username}?start={m.chat.id}_{m.message_id}" if not DB_CHANNEL_ID else f"https://t.me/{bot.username}?start={m.chat.id}_{msg.message_id}"
+    url = f"https://telegram.me/{bot.username}?start={m.chat.id}_{m.message_id}" if not DB_CHANNEL_ID else f"https://telegram.me/{bot.username}?start={m.chat.id}_{msg.message_id}"
     txt = urllib.parse.quote(text.replace('--', ''))
     share_url = f"tg://share?url={txt}File%20Link%20👉%20{url}"
 
@@ -103,7 +103,7 @@ async def storefile_channel(c, m):
 
     # creating urls
     bot = await c.get_me()
-    url = f"https://t.me/{bot.username}?start={m.chat.id}_{m.message_id}" if not DB_CHANNEL_ID else f"https://t.me/{bot.username}?start={m.chat.id}_{msg.message_id}"
+    url = f"https://telegram.me/{bot.username}?start={m.chat.id}_{m.message_id}" if not DB_CHANNEL_ID else f"https://telegram.me/{bot.username}?start={m.chat.id}_{msg.message_id}"
     txt = urllib.parse.quote(text.replace('--', ''))
     share_url = f"tg://share?url={txt}File%20Link%20👉%20{url}"
 
